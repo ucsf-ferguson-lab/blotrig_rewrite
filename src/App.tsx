1625e166f1bb2ed462bda7c46baa0df49c35aae3
kenf1 <103538771+kenf1@users.noreply.github.com>
2025-09-24 17:40:59 +0000

    Add about page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 import { GelMain } from "./routes/GelMain";
 import { ErrorPage } from "./routes/ErrorPage";
+import { AboutPage } from "./routes/AboutPage";
 
 export function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/">
-          <Route index element={<GelMain />} />
+          <Route index element={<AboutPage />} />
+          <Route path="create" element={<GelMain />} />
           <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
