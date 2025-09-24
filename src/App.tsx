f30e2b82be07681ba6152feadd5fab47ada40914
kenf1 <103538771+kenf1@users.noreply.github.com>
2025-09-24 16:17:36 +0000

    Move to repo root dir

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 import { GelMain } from "./routes/GelMain";
 import { ErrorPage } from "./routes/ErrorPage";
-import { GelCreate } from "../../Dev/prior/GelCreate";
 
 export function App() {
   return (
@@ -10,7 +9,6 @@ export function App() {
       <Routes>
         <Route path="/">
           <Route index element={<GelMain />} />
-          <Route path="create" element={<GelCreate />} />
           <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
